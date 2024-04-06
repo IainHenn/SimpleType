1037321443d505cc0484f876e9776d87477d572b
Iain Hennington <133074736+IainHenn@users.noreply.github.com>
2024-04-06 00:03:25 -0700

    Add files via upload

--- WPM Program/main.py
+++ WPM Program/main.py
@@ -1,10 +1,45 @@
 from sentences import words 
+from PySide6 import QtCore, QtWidgets, QtGui
+import sys 
+import random 
 
 wordsTXT = r"words.txt"
 sentence = []
 setOfWords = []
-obj1 = words(setOfWords,sentence)
-obj1.addWords(wordsTXT)
-obj1.create_sentence()
-obj1.getSentence()
-obj1.printSentence()
+
+
+
+#obj1 = words(setOfWords,sentence)
+#obj1.addWords(wordsTXT)
+#obj1.create_sentence()
+#obj1.getSentence()
+#obj1.printSentence()
+
+
+
+class Main(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SimpleType")
+        self.kod = []
+        self.button = QtWidgets.QPushButton("Press Space To Start")
+        lay = QtWidgets.QVBoxLayout(self)
+        lay.setContentsMargins(300,300,300,300)
+        lay.addStretch()
+        lay.addWidget(self.button, alignment = QtCore.Qt.AlignVCenter)
+
+        self.button.clicked.connect(self.clearLayout)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    widget = Main()
+    widget.resize(900,600)
+    widget.show()
+
+    sys.exit(app.exec())
